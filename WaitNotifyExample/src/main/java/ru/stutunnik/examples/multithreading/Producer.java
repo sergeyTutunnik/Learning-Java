d2dead1f93c53ba828821f5040394fc9da34ef5a
mac <sergey.tutunnik@gmail.com>
2019-05-02 00:30:28 +0300

    multithreading example. wait/notify. slow down producer.

--- WaitNotifyExample/src/main/java/ru/stutunnik/examples/multithreading/Producer.java
+++ WaitNotifyExample/src/main/java/ru/stutunnik/examples/multithreading/Producer.java
@@ -11,8 +11,14 @@ public class Producer implements Runnable {
     }
 
     public void run() {
+
         int i = 0;
         while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             q.put(i++);
         }
     }
